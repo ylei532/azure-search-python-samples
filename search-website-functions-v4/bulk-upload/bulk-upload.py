838e191a9d634755336886af51db131245f7983f
ylei532 <80793740+ylei532@users.noreply.github.com>
2022-12-19 13:24:27 +1300

    Commit made from VS Code Azure Static Web Apps

--- search-website-functions-v4/bulk-upload/bulk-upload.py
+++ search-website-functions-v4/bulk-upload/bulk-upload.py
@@ -17,8 +17,8 @@ from azure.search.documents.indexes.models import (
 )
 
 # Get the service name (short name) and admin API key from the environment
-service_name = 'YOUR-SEARCH-SERVICE-NAME'
-key = 'YOUR-SEARCH-SERVICE-ADMIN-API-KEY'
+service_name = 'acshazardoussubstancetooldev'
+key = 'g8rcCdLBv9Jv40rk5v1eUp2AuOFt1w2CY8t5Reqq4QAzSeBBSCAZ'
 endpoint = "https://{}.search.windows.net/".format(service_name)
 
 # Give your index a name
